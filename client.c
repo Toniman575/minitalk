5196c4f1ec081aac41504ee6b42b428722926457
Anton Sadik <asadik@c3r2p1.42vienna.com>
2026-03-24 14:32:08 +0100

    Everything should be done.

--- client.c
+++ client.c
@@ -6,19 +6,59 @@
 /*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 17:07:30 by asadik            #+#    #+#             */
-/*   Updated: 2026/03/22 18:02:57 by asadik           ###   ########.fr       */
+/*   Updated: 2026/03/24 14:31:33 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "client.h"
+#include "libft/error_handling.h"
+#include "libft/ft_printf/ft_printf.h"
 #include "libft/libft.h"
+
 #include <signal.h>
+#include <unistd.h>
+
+t_state	g_state;
+
+void	send_char(int sig)
+{
+	int	index;
+	int	bit;
+
+	(void)sig;
+	index = g_state.index;
+	bit = g_state.bit;
+	g_state.bit++;
+	if (g_state.bit == 8)
+	{
+		g_state.index++;
+		g_state.bit = 0;
+	}
+	if ((g_state.str[index] >> bit) & 1)
+		kill(g_state.server_pid, SIGUSR2);
+	else
+		kill(g_state.server_pid, SIGUSR1);
+}
 
 int	main(int argc, char **argv)
 {
-	int	server_pid;
+	t_result	server_pid;
 
-	(void)argc;
-	server_pid = ft_atoi(argv[1]).value.n;
-	kill(server_pid, SIGUSR1);
-	kill(server_pid, SIGUSR2);
+	signal(SIGUSR1, send_char);
+	if (argc != 3)
+		return (ft_printf("Wrong number of Arguments"), 0);
+	server_pid = ft_atoi(argv[1]);
+	if (server_pid.type == INT)
+	{
+		g_state.server_pid = server_pid.value.n;
+		g_state.bit = 0;
+		g_state.index = 0;
+		g_state.str = argv[2];
+		send_char(SIGUSR1);
+	}
+	else
+		return (ft_printf("Server PID Error: %s\n", server_pid.value.error)
+			, 0);
+	while (g_state.str[g_state.index])
+		pause();
 }
--- server.c
+++ server.c
@@ -6,40 +6,55 @@
 /*   By: asadik <asadik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 16:11:48 by asadik            #+#    #+#             */
-/*   Updated: 2026/03/22 18:06:42 by asadik           ###   ########.fr       */
+/*   Updated: 2026/03/24 14:29:15 by asadik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/ft_printf/ft_printf.h"
+#include "libft/libft.h"
+#include "server.h"
 
 #include <unistd.h>
 #include <signal.h>
 
+t_state	g_state;
 
-void signal_handler(int signum, siginfo_t *info, void *context)
+void	signal_handler(int signum, siginfo_t *info, void *_)
 {
-	(void)context;
+	(void)_;
+	if (info->si_pid != g_state.client_pid)
+	{
+		g_state.client_pid = info->si_pid;
+		g_state.bit = 0;
+		g_state.c = 0;
+	}
 	if (signum == SIGUSR1)
-		ft_printf("Received bit 0 from PID %d\n", info->si_pid);
+		g_state.c = g_state.c & ~(1 << g_state.bit);
 	else if (signum == SIGUSR2)
-		ft_printf("Received bit 1 from PID %d\n", info->si_pid);
+		g_state.c = g_state.c | (1 << g_state.bit);
+	g_state.bit++;
+	if (g_state.bit == 8)
+	{
+		ft_putchar_fd(g_state.c, 1);
+		g_state.c = 0;
+		g_state.bit = 0;
+	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
 	struct sigaction	sa;
-	
+
 	ft_printf("%d\n", getpid());
 	sa.sa_sigaction = signal_handler;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
-
-	
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	
+	g_state.bit = 0;
+	g_state.c = 0;
 	while (1)
 		pause();
-	
-	return 0;
+	return (0);
 }
